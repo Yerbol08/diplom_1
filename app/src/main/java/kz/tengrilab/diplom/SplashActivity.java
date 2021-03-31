f9796d761b08309305b09a2ee4c2a8a4f7e63bb5
Your Name <you@example.com>
2021-03-31 19:11:23 +0600

    finish 3

--- app/src/main/java/kz/tengrilab/diplom/SplashActivity.java
+++ app/src/main/java/kz/tengrilab/diplom/SplashActivity.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        TextView textView = (TextView) findViewById(R.id.textView);
 
+        textView.setVisibility(View.GONE);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
@@ -24,5 +28,16 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+
+        new  Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                textView.setVisibility(View.VISIBLE);
+            }
+        }, 1000);
     }
+
+
+
 }
